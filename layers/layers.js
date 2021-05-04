f377c8036e128e4c914a2c0bd4f5edb4484d0c50
Ryan Tuck <68603989+rtuck1998@users.noreply.github.com>
2021-05-04 15:07:09 -0230

    Add files via upload

---
+++ layers/layers.js
@@ -0,0 +1,161 @@
+var wms_layers = [];
+
+var lyr_newfoundland_modified_0 = new ol.layer.Image({
+                            opacity: 1,
+                            title: "newfoundland_modified",
+                            
+                            
+                            source: new ol.source.ImageStatic({
+                               url: "./layers/newfoundland_modified_0.png",
+    attributions: ' ',
+                                projection: 'EPSG:3857',
+                                alwaysInRange: true,
+                                imageExtent: [-7037011.911946, 5709532.745171, -5469201.289562, 7010736.460481]
+                            })
+                        });
+var lyr_northern_peninsula_modified_1 = new ol.layer.Image({
+                            opacity: 1,
+                            title: "northern_peninsula_modified",
+                            
+                            
+                            source: new ol.source.ImageStatic({
+                               url: "./layers/northern_peninsula_modified_1.png",
+    attributions: ' ',
+                                projection: 'EPSG:3857',
+                                alwaysInRange: true,
+                                imageExtent: [-6783977.620493, 6179054.422385, -5593439.307071, 6923579.103486]
+                            })
+                        });
+var lyr_north_coast_modified_2 = new ol.layer.Image({
+                            opacity: 1,
+                            title: "north_coast_modified",
+                            
+                            
+                            source: new ol.source.ImageStatic({
+                               url: "./layers/north_coast_modified_2.png",
+    attributions: ' ',
+                                projection: 'EPSG:3857',
+                                alwaysInRange: true,
+                                imageExtent: [-6305108.812011, 6249604.129317, -5857894.581041, 6553258.759170]
+                            })
+                        });
+var lyr_southern_shore_modified_3 = new ol.layer.Image({
+                            opacity: 1,
+                            title: "southern_shore_modified",
+                            
+                            
+                            source: new ol.source.ImageStatic({
+                               url: "./layers/southern_shore_modified_3.png",
+    attributions: ' ',
+                                projection: 'EPSG:3857',
+                                alwaysInRange: true,
+                                imageExtent: [-6215758.882844, 5759982.848717, -5764317.099167, 6152260.959369]
+                            })
+                        });
+var lyr_straits_modified_4 = new ol.layer.Image({
+                            opacity: 1,
+                            title: "straits_modified",
+                            
+                            
+                            source: new ol.source.ImageStatic({
+                               url: "./layers/straits_modified_4.png",
+    attributions: ' ',
+                                projection: 'EPSG:3857',
+                                alwaysInRange: true,
+                                imageExtent: [-6531804.374992, 6506303.989190, -6069817.614787, 6869422.659654]
+                            })
+                        });
+var lyr_st_georges_modified_5 = new ol.layer.Image({
+                            opacity: 1,
+                            title: "st_georges_modified",
+                            
+                            
+                            source: new ol.source.ImageStatic({
+                               url: "./layers/st_georges_modified_5.png",
+    attributions: ' ',
+                                projection: 'EPSG:3857',
+                                alwaysInRange: true,
+                                imageExtent: [-6715554.470239, 6027995.349955, -6408304.976451, 6320168.691136]
+                            })
+                        });
+var lyr_placentia_modified_6 = new ol.layer.Image({
+                            opacity: 1,
+                            title: "placentia_modified",
+                            
+                            
+                            source: new ol.source.ImageStatic({
+                               url: "./layers/placentia_modified_6.png",
+    attributions: ' ',
+                                projection: 'EPSG:3857',
+                                alwaysInRange: true,
+                                imageExtent: [-6216644.623512, 5839671.930666, -5951785.200034, 6145722.189811]
+                            })
+                        });
+var lyr_capespear_to_capebonavista_modified2_7 = new ol.layer.Image({
+                            opacity: 1,
+                            title: "capespear_to_capebonavista_modified2",
+                            
+                            
+                            source: new ol.source.ImageStatic({
+                               url: "./layers/capespear_to_capebonavista_modified2_7.png",
+    attributions: ' ',
+                                projection: 'EPSG:3857',
+                                alwaysInRange: true,
+                                imageExtent: [-6040682.703529, 5986310.141631, -5819500.772555, 6254838.783471]
+                            })
+                        });
+var lyr_west_coast_modified_8 = new ol.layer.Image({
+                            opacity: 1,
+                            title: "west_coast_modified",
+                            
+                            
+                            source: new ol.source.ImageStatic({
+                               url: "./layers/west_coast_modified_8.png",
+    attributions: ' ',
+                                projection: 'EPSG:3857',
+                                alwaysInRange: true,
+                                imageExtent: [-6597847.396441, 6227193.420611, -6348717.468654, 6506820.259144]
+                            })
+                        });
+var lyr_fogo_modified_9 = new ol.layer.Image({
+                            opacity: 1,
+                            title: "fogo_modified",
+                            
+                            
+                            source: new ol.source.ImageStatic({
+                               url: "./layers/fogo_modified_9.png",
+    attributions: ' ',
+                                projection: 'EPSG:3857',
+                                alwaysInRange: true,
+                                imageExtent: [-6066758.327735, 6347818.043069, -5969747.438720, 6425740.737244]
+                            })
+                        });
+var lyr_south_coast_burin_modified_10 = new ol.layer.Image({
+                            opacity: 1,
+                            title: "south_coast_burin_modified",
+                            
+                            
+                            source: new ol.source.ImageStatic({
+                               url: "./layers/south_coast_burin_modified_10.png",
+    attributions: ' ',
+                                projection: 'EPSG:3857',
+                                alwaysInRange: true,
+                                imageExtent: [-6738467.128157, 5833149.457738, -6125106.526549, 6151665.226636]
+                            })
+                        });
+var lyr_south_coast_modified_11 = new ol.layer.Image({
+                            opacity: 1,
+                            title: "south_coast_modified",
+                            
+                            
+                            source: new ol.source.ImageStatic({
+                               url: "./layers/south_coast_modified_11.png",
+    attributions: ' ',
+                                projection: 'EPSG:3857',
+                                alwaysInRange: true,
+                                imageExtent: [-6732660.824003, 5826529.645602, -6284448.813852, 6145724.893681]
+                            })
+                        });
+
+lyr_newfoundland_modified_0.setVisible(true);lyr_northern_peninsula_modified_1.setVisible(true);lyr_north_coast_modified_2.setVisible(true);lyr_southern_shore_modified_3.setVisible(true);lyr_straits_modified_4.setVisible(true);lyr_st_georges_modified_5.setVisible(true);lyr_placentia_modified_6.setVisible(true);lyr_capespear_to_capebonavista_modified2_7.setVisible(true);lyr_west_coast_modified_8.setVisible(true);lyr_fogo_modified_9.setVisible(true);lyr_south_coast_burin_modified_10.setVisible(true);lyr_south_coast_modified_11.setVisible(true);
+var layersList = [lyr_newfoundland_modified_0,lyr_northern_peninsula_modified_1,lyr_north_coast_modified_2,lyr_southern_shore_modified_3,lyr_straits_modified_4,lyr_st_georges_modified_5,lyr_placentia_modified_6,lyr_capespear_to_capebonavista_modified2_7,lyr_west_coast_modified_8,lyr_fogo_modified_9,lyr_south_coast_burin_modified_10,lyr_south_coast_modified_11];
